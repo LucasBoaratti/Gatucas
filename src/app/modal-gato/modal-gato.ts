e93d80df423ab216d1191154d5f9022ae51e476e
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-09-15 13:06:02 -0300

    Modal finalizado.

--- src/app/modal-gato/modal-gato.ts
+++ src/app/modal-gato/modal-gato.ts
@@ -4,14 +4,18 @@ import { App } from '../app';
 
 @Component({
   selector: 'app-modal-gato',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './modal-gato.html',
   styleUrl: './modal-gato.scss'
 })
 export class ModalGato {
+    // Definindo o estado do modal como false
     @Input() open = false;
 
-    @Input() fecharModalGato!: () => void;
+    // Definindo a função para fechar o modal
+    @Input() closeModalGato!: () => void;
 
+    // Definindo a URL da imagem do gato como vazio
     @Input() imagemGato = "";
 }
